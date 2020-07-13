c47816564a1dd14dc61c190ba5a6a5cb0ba8a91f
Griffin <60614863+h-griffin@users.noreply.github.com>
2020-07-13 15:14:23 -0700

    detail not working

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,6 @@
-from rest_framework.generics import ListAPIView, RetrieveUpdateDestroyAPIView, ListCreateAPIView #, APIView
+from rest_framework.generics import ListAPIView, RetrieveUpdateDestroyAPIView, ListCreateAPIView  #, APIView
+from django.views.generic.detail import DetailView
+
 
 from .models import Recipe
 from .serializers import RecipesSerializer
@@ -9,10 +11,15 @@ class RecipesList(ListAPIView):
     queryset = Recipe.objects.all()
     serializer_class = RecipesSerializer
 
-class RecipesDetail(RetrieveUpdateDestroyAPIView):
+class RecipesDetail(DetailView):
     permission_classes = (ISAuthorOrReadOnly)
     queryset = Recipe.objects.all()
     serializer_class = RecipesSerializer
+
+# class RecipesDetail(RetrieveUpdateDestroyAPIView):
+#     permission_classes = (ISAuthorOrReadOnly)
+#     queryset = Recipe.objects.all()
+#     serializer_class = RecipesSerializer
 
 # class RecipeCreate(ListCreateAPIView):
 
