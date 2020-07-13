27bd495da959c2aacda5357a2d0dc4dcf4898a2a
Griffin <60614863+h-griffin@users.noreply.github.com>
2020-07-13 15:17:21 -0700

    removed detail view

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,5 @@
 from rest_framework.generics import ListAPIView, RetrieveUpdateDestroyAPIView, ListCreateAPIView  #, APIView
-from django.views.generic.detail import DetailView
+# from django.views.generic.detail import DetailView
 
 
 from .models import Recipe
@@ -11,15 +11,10 @@ class RecipesList(ListAPIView):
     queryset = Recipe.objects.all()
     serializer_class = RecipesSerializer
 
-class RecipesDetail(DetailView):
+class RecipesDetail(RetrieveUpdateDestroyAPIView):
     permission_classes = (ISAuthorOrReadOnly)
     queryset = Recipe.objects.all()
     serializer_class = RecipesSerializer
-
-# class RecipesDetail(RetrieveUpdateDestroyAPIView):
-#     permission_classes = (ISAuthorOrReadOnly)
-#     queryset = Recipe.objects.all()
-#     serializer_class = RecipesSerializer
 
 # class RecipeCreate(ListCreateAPIView):
 
