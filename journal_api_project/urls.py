e4ff82eb839e8bd2a62f6f4139b1b28e6963f6f3
Griffin <60614863+h-griffin@users.noreply.github.com>
2020-07-13 20:53:03 -0700

    login error 500

--- journal_api_project/urls.py
+++ journal_api_project/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('', include('recipes.urls'), name='home'),
     path('', include('django.contrib.auth.urls')),
     path('api-auth/', include('rest_framework.urls')), 
+    path('/api-auth/login/', include('recipes.urls'), name='home'),
     path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
 ]
