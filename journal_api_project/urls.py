08b9a4e4635478c1908df5d01f6119e4eca6f46a
Peng Chen <pchen0816@outlook.com>
2020-07-16 14:54:02 -0700

    auth and route view modified

--- journal_api_project/urls.py
+++ journal_api_project/urls.py
@@ -12,5 +12,5 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')), 
     path('/api-auth/login/', include('users.urls'), name='home'),
     path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
 ]
