9678022a49c84dd852baf300850bb279a331d105
Peng Chen <pchen0816@outlook.com>
2020-07-16 23:06:40 -0700

    users auth and view bug fixed

--- users/views.py
+++ users/views.py
@@ -1,25 +1,13 @@
-from django.urls import reverse_lazy
-from django.views.generic.edit import CreateView
-from django.contrib.auth.forms import UserCreationForm
-from rest_framework import generics
-from django.contrib.auth.models import User 
-from rest_framework.generics import ListAPIView
-
-from .forms import CustomUserCreationForm
+from rest_framework.generics import ListAPIView, CreateAPIView
 from .models import CustomUser
-from .serializers import UserSerializer, CustomUserSerializer 
+from .serializers import UserCreateSerializer, UserListSerializer 
 
-class SignUpView(CreateView):
-    form_class = CustomUserCreationForm
-    success_url = reverse_lazy('recipes_list')
-    serializer_class = UserSerializer
-    # redirect_field_name = 'home'
 
-class UserCreate(generics.CreateAPIView):
+class UserCreate(CreateAPIView):
     queryset = CustomUser.objects.all()
-    serializer_class = CustomUserSerializer
+    serializer_class = UserCreateSerializer
     # permission_classes = ()
 
 class UserListView(ListAPIView):
     queryset = CustomUser.objects.all()
-    serializer_class = CustomUserSerializer
+    serializer_class = UserListSerializer
