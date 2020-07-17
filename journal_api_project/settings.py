e3b2a807d7f1a2870e0bcb9daef38dd05d31659b
Peng Chen <pchen0816@outlook.com>
2020-07-17 13:22:25 -0700

    bug fix

--- journal_api_project/settings.py
+++ journal_api_project/settings.py
@@ -170,9 +170,9 @@ REST_FRAMEWORK = {
     # 'DEFAULT_PERMISSION_CLASSES': [
     #     'rest_framework.permissions.IsAuthenticated',
     # ],
-    # 'DEFAULT_PERMISSION_CLASSES': [
-        # 'rest_framework.permissions.IsAuthenticatedOrReadOnly',
-    # ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    ],
 
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
